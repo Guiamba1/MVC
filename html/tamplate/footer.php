2dabb70a27e19576c18a3668adff59e8f0c25d2f
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-10-13 22:19:09 -0700

    visual
    Mudancas no visual da index

--- html/tamplate/footer.php
+++ html/tamplate/footer.php
@@ -1,14 +1,15 @@
 
 <footer class="mbr-section mbr-section--relative mbr-section--fixed-size mbr-parallax-background" id="footer1-4" style="background-image: url(views/images/bg3.jpg);">
-    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(68, 68, 68);"></div>
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: #3586ff; "></div>
     <div class="mbr-section__container container">
         <div class="mbr-footer mbr-footer--wysiwyg row">
             <div class="col-sm-12">
-                <p class="mbr-footer__copyright">Copyright (c) <?php echo date("l")." " .date("Y/m/d") ; ?> Oscar Alberto Guiamba. <a class="mbr-footer__link text-gray" href="#/">Termos de Uso</a>  | <a class="mbr-footer__link text-gray" href="#/">Política de Privacidade</a></p>
+                <p class="mbr-footer__copyright" >Copyright (c) <?php echo date("l")." " .date("Y/m/d") ; ?> Oscar Alberto Guiamba. </p>
             </div>
         </div>
     </div>
 </footer>
+
 <script src="views/web/assets/jquery/jquery.min.js"></script>
   <script src="views/bootstrap/js/bootstrap.min.js"></script>
   <script src="views/smooth-scroll/SmoothScroll.js"></script>
--- rascunhos.php
+++ rascunhos.php
@@ -2,3 +2,22 @@
 
 # INSERT INTO `usuario` (`id`, `user`, `pass`, `email`, `permissoes`) 
 VALUES (NULL, 'oscar', '1234', 'oscar@gmail.com', '2');
+<div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(68, 68, 68);"></div>
+    <div class="mbr-section__container container">
+        <div class="mbr-footer mbr-footer--wysiwyg row">
+            <div class="col-sm-12">
+            <div class="ondas">
+                <div class="onda" id="onda1"></div>
+                <div class="onda" id="onda2"></div>
+                <div class="onda" id="onda3"></div>
+                <div class="onda" id="onda4"></div>
+        
+            </div>
+                <p class="mbr-footer__copyright">Copyright (c) <?php echo date("l")." " .date("Y/m/d") ; ?> Oscar Alberto Guiamba. <a class="mbr-footer__link text-gray" href="#/">Termos de Uso</a>  | <a class="mbr-footer__link text-gray" href="#/">Política de Privacidade</a></p>
+            </div>
+        </div>
+    </div 845556468> # 39055381741286
+
+    <footer class="mbr-section mbr-section--relative mbr-section--fixed-size mbr-parallax-background" id="footer1-4" style="background-image: url(views/images/bg3.jpg);">
+    
+    style="color: #fff;  text-align: center;"
